8db9d530423160a92168e5b78fb7af78dd9a4ed1
Adrian Arco Gonzalez <adrian.arco.gonzalez@gmail.com>
2024-02-08 14:30:58 +0100

    Add title on app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,6 +5,7 @@ import RandomBeerSection from "../RandomBeerSection/RandomBeerSection";
 const App = (): JSX.Element => {
   return (
     <AppStyled>
+      <h1 className="title">Beer App</h1>
       <RandomBeerSection />
       <SearcherBeers />
     </AppStyled>
